5368682ae0346b70ab4aa471f215e42463e35690
Krzysztof Stachowiak <krzysiek.stachowiak#gmail.com>
2014-10-23 21:33:03 +0200

    Slight database refactoring.

--- src/kernel/database.c
+++ src/kernel/database.c
@@ -6,26 +6,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct ConfigNodeInt {
+struct ConfigNode {
     char *key;
-    long value;
-    struct ConfigNodeInt *next;
+    union { double d; long l; } value;
+    struct ConfigNode *next;
 };
 
-struct ConfigNodeReal {
-    char *key;
-    double value;
-    struct ConfigNodeReal *next;
-};
+static struct ConfigNode *integers;
+static struct ConfigNode *reals;
 
-static struct ConfigNodeInt *integers;
-static struct ConfigNodeReal *reals;
-
-static void db_insert_integer(char *key, long value)
+static struct ConfigNode *db_make_node(char *key)
 {
     int key_len;
     char *key_copy;
-    struct ConfigNodeInt *new_node;
+    struct ConfigNode *new_node;
 
     key_len = strlen(key);
     key_copy = malloc(key_len + 1);
@@ -42,33 +36,44 @@ static void db_insert_integer(char *key, long value)
     }
 
     new_node->key = key_copy;
-    new_node->value = value;
+    
+    return new_node;
+}
+
+static struct ConfigNode *db_find(struct ConfigNode *list, char *key)
+{
+    struct ConfigNode *current;
+    for (current = list; current; current = current->next) {
+        if (strcmp(key, current->key) == 0) {
+            return current;
+        }
+    }
+    return NULL;
+}
+
+static void db_free_list(struct ConfigNode *list)
+{
+    struct ConfigNode *temp;
+    while (list) {
+        temp = list;
+        list = list->next;
+        free(temp->key);
+        free(temp);
+    }
+}
+
+static void db_insert_integer(char *key, long value)
+{
+    struct ConfigNode *new_node = db_make_node(key);
+    new_node->value.l = value;
     new_node->next = integers;
     integers = new_node;
 }
 
 static void db_insert_real(char *key, double value)
 {
-    int key_len;
-    char *key_copy;
-    struct ConfigNodeReal *new_node;
-
-    key_len = strlen(key);
-    key_copy = malloc(key_len + 1);
-    if (!key_copy) {
-        DIAG_ERROR("Allocation failed.");
-        exit(1);
-    }
-    memcpy(key_copy, key, key_len + 1);
-
-    new_node = malloc(sizeof(*new_node));
-    if (!new_node) {
-        DIAG_ERROR("Allocation failed.");
-        exit(1);
-    }
-
-    new_node->key = key_copy;
-    new_node->value = value;
+    struct ConfigNode *new_node = db_make_node(key);
+    new_node->value.d = value;
     new_node->next = reals;
     reals = new_node;
 }
@@ -82,48 +87,29 @@ void db_init(void)
 
 void db_deinit(void)
 {
-    struct ConfigNodeInt *itemp;
-    struct ConfigNodeReal *rtemp;
-
-    while (integers) {
-        itemp = integers->next;
-        free(integers->key);
-        free(integers);
-        integers = itemp;
-    }
-
-    while (reals) {
-        rtemp = reals->next;
-        free(reals->key);
-        free(reals);
-        reals = rtemp;
-    }
+    db_free_list(integers);
+    db_free_list(reals);
 }
-
 
 long db_integer(char *key)
 {
-    struct ConfigNodeInt *current;
-    for (current = integers; current; current = current->next) {
-        if (strcmp(key, current->key) == 0) {
-            return current->value;
-        }
+    struct ConfigNode *found = db_find(integers, key);
+    if (!found) {
+        DIAG_ERROR("Invalid key \"%s\" requested in INTEGERS.", key);
+        exit(1);
+    } else {
+        return found->value.l;
     }
-
-    DIAG_ERROR("Invalid key requested.");
-    exit(1);
 }
 
 double db_real(char *key)
 {
-    struct ConfigNodeReal *current;
-    for (current = reals; current; current = current->next) {
-        if (strcmp(key, current->key) == 0) {
-            return current->value;
-        }
+    struct ConfigNode *found = db_find(reals, key);
+    if (!found) {
+        DIAG_ERROR("Invalid key \"%s\" requested in REALS.", key);
+        exit(1);
+    } else {
+        return found->value.d;
     }
-
-    DIAG_ERROR("Invalid key requested.");
-    exit(1);
 }
 
